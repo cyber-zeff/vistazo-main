76a1894c90b729c0e6bc9e0161e70bef11e2f17f
cyber-zeff <huzaifashamsi819@gmail.com>
2026-02-14 13:32:39 +0500

    fixed text-sizing and on tap animation glitches

--- app/components/Services.tsx
+++ app/components/Services.tsx
@@ -56,10 +56,16 @@ function ServiceItem({
             initial="rest"
             animate={isActive ? "hover" : "rest"}
             whileHover="hover"
-            onTap={() =>
-                setActiveIndex(isActive ? null : index)
-            }
-            className="group relative mt-4 md:mt-8 flex items-center justify-between px-0 md:px-3 py-6 md:py-8 cursor-pointer transition-colors duration-300 hover:bg-[rgba(103,85,207,0.75)] focus:bg-[rgba(103,85,207,0.75)] rounded-[15px]"
+            onPointerDown={() => {
+                if (window.innerWidth < 1024) setActiveIndex(index)  // open on press
+            }}
+            onPointerUp={() => {
+                if (window.innerWidth < 1024) setActiveIndex(null)  // close on release
+            }}
+            onPointerLeave={() => {
+                if (window.innerWidth < 1024) setActiveIndex(null)  // close if finger leaves the element
+            }}
+            className="group relative mt-4 md:mt-8 flex items-center justify-between px-0 md:px-3 py-6 md:py-8 cursor-pointer transition-colors duration-300 hover:bg-[rgba(103,85,207,0.75)] max-lg:active:bg-[rgba(103,85,207,0.75)] rounded-[15px]"
         >
             <div className="sm:flex sm:items-center sm:gap-4 md:gap-6 max-sm:mx-auto">
                 <motion.div
@@ -68,12 +74,12 @@ function ServiceItem({
                         hover: { rotate: 180 },
                     }}
                     transition={{ duration: 0.4, ease: "easeOut" }}
-                    className="text-white group-hover:text-[#F9D94D] group-focus:text-[#F9D94D]"
+                    className="text-white group-hover:text-[#F9D94D] max-lg:group-active:text-[#F9D94D]"
                 >
                     <DiamondStar />
                 </motion.div>
 
-                <h3 className="text-[clamp(20px,6vw,48px)] quantaFont tracking-tight group-hover:text-[#F9D94D] group-focus:text-[#F9D94D]">
+                <h3 className="text-[clamp(20px,6vw,48px)] quantaFont tracking-tight group-hover:text-[#F9D94D] max-lg:group-active:text-[#F9D94D]">
                     {service.title}
                 </h3>
             </div>
@@ -143,7 +149,7 @@ function ServiceItem({
 
 
             {/* Bottom Divider */}
-            <div className="absolute left-0 right-0 -bottom-3 h-[0.5px] bg-gray-300/80 pointer-events-none transition-opacity duration-300 group-hover:opacity-0 group-focus:opacity-0" />
+            <div className="absolute left-0 right-0 -bottom-3 h-[0.5px] bg-gray-300/80 pointer-events-none transition-opacity duration-300 group-hover:opacity-0 max-lg:group-active:opacity-0" />
         </motion.div>
     )
 }
@@ -156,7 +162,7 @@ export default function Services() {
         <>
             <section id="services" className="px-0 sm:px-10 md:px-20 md:py-24">
                 <div className="max-w-7xl mx-auto mb-12 max-sm:px-10 max-sm:text-center">
-                    <h2 className="text-[clamp(30px,8vw,96px)] mt-16 md:mt-0 quantaFont leading-normal">
+                    <h2 className="text-[clamp(30px,9vw,64px)] sm:text-[clamp(50px,8vw,96px)] mt-16 md:mt-0 quantaFont leading-normal">
                         What we serve <br className="block sm:hidden" /> on <br className="hidden sm:block" /> the <span className="text-[#F9D94D]">menu</span>
                     </h2>
                     <p className="text-[14px] sm:text-[clamp(14px,6vw,20px)] font-medium">
@@ -178,7 +184,7 @@ export default function Services() {
             </section>
 
             <div className="bg-[#FFFEF7] w-full m-0 p-0 overflow-hidden pb-10 md:pb-20">
-                <svg xmlns="http://www.w3.org/2000/svg" width="1440" height="175" viewBox="0 0 1440 175" fill="none" className="max-md:relative max-md:-left-44 lg:w-full h-32.75 lg:h-auto">
+                <svg xmlns="http://www.w3.org/2000/svg" width="1440" height="175" viewBox="0 0 1440 175" fill="none" className="max-md:relative max-md:-left-44 md:w-full h-32.75 md:h-auto">
                     <path d="M-144 0V174.661C-108.583 171.493 -81.339 147.058 -76.3442 112.217C-64.0845 195.474 55.7888 195.474 68.0486 112.217C81.2164 195.474 200.182 196.379 211.987 112.217C223.793 195.927 344.12 195.474 355.926 112.217C368.186 195.927 488.513 195.022 500.319 112.217C513.033 196.832 633.814 194.57 644.258 112.217C649.252 194.117 781.385 196.832 788.196 112.217C800.91 195.475 919.875 195.927 932.589 112.217C945.303 195.927 1064.96 195.415 1076.53 112.217C1088.79 195.928 1206.39 195.928 1220.47 112.217C1234.54 195.928 1351.69 195.475 1364.86 112.217C1375.76 194.57 1497.45 196.38 1508.8 112.217C1513.34 144.796 1539.22 171.493 1576 174.661V0L-144 0Z" fill="#361E98" />
                 </svg>
             </div>
