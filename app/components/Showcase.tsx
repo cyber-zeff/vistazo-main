ccd24e7d091111f9973cf61042894e68c0c0de91
cyber-zeff <huzaifashamsi819@gmail.com>
2026-03-02 18:16:07 +0500

    add: safari compatibility for hero

--- app/components/Showcase.tsx
+++ app/components/Showcase.tsx
@@ -35,13 +35,14 @@ export default function Showcase() {
           playsInline
           disablePictureInPicture
           controlsList="nodownload nofullscreen noremoteplayback"
-          className="block mx-auto h-full w-full max-w-[640px] object-contain bg-main"
+          className="block mx-auto h-full w-full max-w-[640px] object-contain"
         >
+          <source src="/hero-mobile.mov" type="video/quicktime" />
           <source src="/hero-animation-mobile.webm" type="video/webm" />
         </video>
       </section>
 
-      <div className="max-lg:hidden absolute bottom-0 right-0 z-0 pointer-events-none">
+      <div className="max-sm:hidden absolute bottom-0 right-0 z-0 pointer-events-none">
         <svg xmlns="http://www.w3.org/2000/svg" width="1014" height="211" viewBox="0 0 1014 211" fill="none">
           <path d="M1013.2 0.5C614.199 0.5 360.988 53.9068 0.199219 210.5M1013.2 18.8807C715.199 18.8807 518.94 69.912 263.199 210.5M1013.2 37.2615C808.699 37.2615 664.508 87.6972 509.699 210.5M1013.2 55.6422C896.699 55.6422 787.833 116.429 732.199 210.5M1013.2 74.023C958.199 87.567 916.46 190.281 910.699 210.04" stroke="white" />
         </svg>
